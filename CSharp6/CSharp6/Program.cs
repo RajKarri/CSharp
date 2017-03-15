6772d746bf1a13912dbe17ae4866b87410d6bb38
rkarri <rkarri@DEV-VDI-P-054>
2017-03-15 13:53:40 -0500

    String interpolation

--- CSharp6/CSharp6/Program.cs
+++ CSharp6/CSharp6/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CSharp6
 {
@@ -10,6 +6,9 @@ namespace CSharp6
     {
         static void Main(string[] args)
         {
+            // string interpolation
+            StringInterpolate.Sample();
+            Console.Read();
         }
     }
 }
---
+++ CSharp6/CSharp6/StringInterpolate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSharp6
+{
+    public static class StringInterpolate
+    {
+        public static void Sample()
+        {
+            string sampleString = "This is to test string interpolation";
+            Console.WriteLine(string.Format("Before C# 6.0 : {0}", sampleString));
+            Console.WriteLine($"In C# 6.0 : {sampleString}");
+        }
+    }
+}
