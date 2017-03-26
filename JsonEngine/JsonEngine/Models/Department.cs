e1037b7806a1edb0b4bd5631346130da8af7ad3f
Raj <rajkarri85@gmail.com>
2017-03-26 13:58:03 -0400

    Json.Net test examples

---
+++ JsonEngine/JsonEngine/Models/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace JsonEngine.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ JsonEngine/JsonEngine/Models/Professor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace JsonEngine.Models
+{
+    public class Professor
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public IList<Department> Departments { get; set; }
+    }
+}
